2954936f1d8b8bc187c49e587f8ddb3ab168a43c
Khairul Islam <42338379+whoiskhairul@users.noreply.github.com>
2018-10-09 19:58:28 +0600

    learned before midterm.

---
+++ 5 stack.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int stack[20]; //Here we declared the stack and its maxsize.
+int head = -1; //We have take the value of head=-1 to track the position of stack.
+
+//By push function,we will take input of the stack.
+void push(int data){
+head++;
+stack[head] = data;
+}
+
+////By pop function,we will find the last input and output it.
+int pop(){
+int value = stack[head];
+head--;
+return value;
+
+}
+
+//By print function,we will display the total stack.
+void print(){
+    printf("The condition of the stack is: \n");
+    for(int i=0;i<=head;i++){
+            printf("%d ",stack[i]);
+    }
+
+}
+
+void main() //declaration of the main function.
+{
+    push(9);  //giving value to take input.
+    push(6);
+    push(3);
+    push(5);
+    push(8);
+    print();  //calling the print function to display the stack.
+    printf("\n Pop data : %d \n",pop()); //calling pop to find the last input.
+    print();
+}
---
+++ 6 queue.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+
+int queue[10]; //defined the size of queue.
+int head = -1,tail = 0;
+
+//This is enqueue function,by this we will take input.we will check first if the queue is full .
+void enqueue(int data){
+   if(head == 9)
+      printf("Queue is Full.\n");
+   else{
+      head++;
+      queue[head] = data;
+   }
+}
+
+//If the queue is no empty,then this function will give output ( first input ).
+void dequeue(){
+   if(head == -1)
+      printf("Queue is Empty.\n");
+   else{
+      printf("\n Deleted : %d\n", queue[tail]);
+      tail++;
+   }
+}
+
+//This function is to Display the queue.
+void display(){
+   if(head == -1)
+      printf("Queue is Empty.\n");
+   else{
+      int i;
+      printf("Queue elements are:\n");
+      for(i=tail; i<=head; i++)
+	 printf("%d ",queue[i]);
+   }
+}
+
+void main()
+{
+
+enqueue(10);
+enqueue(20);
+enqueue(30);
+display();
+enqueue(40);
+display();
+dequeue();
+display();
+dequeue();
+display();
+}
